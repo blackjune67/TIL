8cf0723a8e7285a5b48d5c142c373144dd211ee4
hajune <buea486@gmail.com>
2024-05-12 19:22:34 +0900

    Spring MVC 기초 요청

---
+++ basicspring/src/main/java/org/basic/spring/requestMapping/HelloData.java
@@ -0,0 +1,11 @@
+package org.basic.spring.requestMapping;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+@Getter
+public class HelloData {
+    private String userName;
+    private int age;
+}
---
+++ basicspring/src/test/java/org/basic/spring/BasicspringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.basic.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
