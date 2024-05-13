cbc4f4099e140b1b4e02f4bf10eecdb0ad8496a0
hajune <buea486@gmail.com>
2024-05-13 22:24:04 +0900

    TIL spring basic

---
+++ basicspring/src/main/java/org/basic/spring/response/ResponseHeaderServlet.java
@@ -0,0 +1,23 @@
+package org.basic.spring.response;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "responseHeaderServlet", urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
+
+    private void content(HttpServletResponse res) {
+        res.setContentType("text/plain");
+    }
+
+}
